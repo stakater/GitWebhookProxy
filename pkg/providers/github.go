dfcb9b47598445c4e7a61f9f06e901e9df31bb7f
hazim1093 <hazim_malik@hotmail.com>
2018-04-19 20:30:54 +0500

    Implement github provider

--- pkg/providers/github.go
+++ pkg/providers/github.go
@@ -1,7 +1,14 @@
 package providers
 
 import (
-	"net/http"
+	"errors"
+	"strings"
+)
+
+const (
+	XGitHubSignature = "X-GitHub-Signature"
+	XGitHubEvent     = "X-GitHub-Event"
+	XGitHubDelivery  = "X-GitHub-Delivery"
 )
 
 type GithubProvider struct {
@@ -9,20 +16,28 @@ type GithubProvider struct {
 }
 
 func NewGithubProvider(secret string) (*GithubProvider, error) {
-	// if secret == nil {
-	// 	return nil, fmt.Errorf("blah ")
-	// }
+	if len(strings.TrimSpace(secret)) == 0 {
+		return nil, errors.New("Cannot create github provider with empty secret")
+	}
 
 	return &GithubProvider{
 		secret: secret,
 	}, nil
 }
 
-func (p *GithubProvider) GetHook(req *http.Request) (Hook, error) {
-
-	return Hook{Id: "lol1"}, nil
+func (p *GithubProvider) GetHeaderKeys() []string {
+	return []string{
+		XGitHubDelivery,
+		XGitHubEvent,
+		XGitHubSignature,
+	}
 }
 
-func (p *GithubProvider) validate() bool {
-	return false
+func (p *GithubProvider) GetTokenHeaderKey() string {
+	return XGitHubSignature
+}
+
+func (p *GithubProvider) Validate(hook Hook) bool {
+	//TODO: Validate
+	return true
 }
