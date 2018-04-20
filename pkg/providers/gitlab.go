2e4ce20bec6a6fc154f43c9b37a32c51574bbd32
hazim1093 <hazim_malik@hotmail.com>
2018-04-20 19:39:16 +0500

    Add gitlab provider implementation

--- pkg/providers/gitlab.go
+++ pkg/providers/gitlab.go
@@ -1,1 +1,42 @@
 package providers
+
+import (
+	"errors"
+	"strings"
+)
+
+// Header constants
+const (
+	XGitlabToken = "X-Gitlab-Token"
+	XGitlabEvent = "X-Gitlab-Event"
+)
+
+type GitlabProvider struct {
+	secret string
+}
+
+func NewGitlabProvider(secret string) (*GitlabProvider, error) {
+	if len(strings.TrimSpace(secret)) == 0 {
+		return nil, errors.New("Cannot create github provider with empty secret")
+	}
+
+	return &GitlabProvider{
+		secret: secret,
+	}, nil
+}
+
+func (p *GitlabProvider) GetHeaderKeys() []string {
+	return []string{
+		XGitlabToken,
+		XGitlabEvent,
+	}
+}
+
+func (p *GitlabProvider) Validate(hook Hook) bool {
+	token := hook.Headers[XGitlabToken]
+	if len(token) <= 0 {
+		return false
+	}
+
+	return strings.TrimSpace(token) == strings.TrimSpace(p.secret)
+}
