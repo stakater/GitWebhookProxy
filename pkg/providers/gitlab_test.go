a52e6b26c0a6813fd1190afbbde3ae85ddaf4b91
hazim1093 <hazim_malik@hotmail.com>
2018-04-23 15:57:43 +0500

    Add tests for gitlab

---
+++ pkg/providers/gitlab_test.go
@@ -0,0 +1,244 @@
+package providers
+
+import (
+	"reflect"
+	"testing"
+)
+
+const (
+	secret = "myGitLabTestSecret"
+)
+
+func TestNewGitlabProvider(t *testing.T) {
+	type args struct {
+		secret string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *GitlabProvider
+		wantErr bool
+	}{
+		{
+			name: "TestNewGitlabProviderWithCorrectSecret",
+			args: args{
+				secret: secret,
+			},
+			want: &GitlabProvider{
+				secret: secret,
+			},
+			wantErr: false,
+		},
+		{
+			name: "TestNewGitlabProviderWithEmptySecret",
+			args: args{
+				secret: "",
+			},
+			wantErr: true,
+		},
+		{
+			name:    "TestNewGitlabProviderWithNoSecret",
+			args:    args{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewGitlabProvider(tt.args.secret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewGitlabProvider() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewGitlabProvider() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGitlabProvider_GetHeaderKeys(t *testing.T) {
+	type fields struct {
+		secret string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			name: "TestGetHeaderKeysWithCorrectValues",
+			want: []string{XGitlabToken, XGitlabEvent},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &GitlabProvider{
+				secret: tt.fields.secret,
+			}
+			if got := p.GetHeaderKeys(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GitlabProvider.GetHeaderKeys() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGitlabProvider_Validate(t *testing.T) {
+	type fields struct {
+		secret string
+	}
+	type args struct {
+		hook Hook
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "TestValidateWithEmptyTokenValue",
+			fields: fields{
+				secret: secret,
+			},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{
+						XGitlabToken: "",
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "TestValidateWithEmptyHeaders",
+			fields: fields{
+				secret: secret,
+			},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "TestValidateWithWrongTokenValue",
+			fields: fields{
+				secret: secret,
+			},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{
+						XGitlabToken: "IncorrectSecret",
+					},
+					Payload: nil,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "TestValidateWithCorrectTokenValue",
+			fields: fields{
+				secret: secret,
+			},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{
+						XGitlabToken: secret,
+					},
+					Payload: nil,
+				},
+			},
+			want: true,
+		},
+		{
+			name: "TestValidateWithWrongHeaderKey",
+			fields: fields{
+				secret: secret,
+			},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{
+						"X-Wrong-Token": "IncorrectSecret",
+					},
+					Payload: nil,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "TestValidateWithNilHeaders",
+			fields: fields{
+				secret: secret,
+			},
+			args: args{
+				hook: Hook{
+					Headers: nil,
+					Payload: nil,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "TestValidateWithNoHookArg",
+			fields: fields{
+				secret: secret,
+			},
+			args: args{},
+			want: false,
+		},
+		{
+			name: "TestValidateWithWrongSecretInProxy",
+			fields: fields{
+				secret: "WrongSecret",
+			},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{
+						XGitlabToken: secret,
+					},
+					Payload: nil,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "TestValidateWithEmptySecretInProxy",
+			fields: fields{
+				secret: "",
+			},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{
+						XGitlabToken: secret,
+					},
+					Payload: nil,
+				},
+			},
+			want: false,
+		},
+		{
+			name:   "TestValidateWithNoSecretInProxy",
+			fields: fields{},
+			args: args{
+				hook: Hook{
+					Headers: map[string]string{
+						XGitlabToken: secret,
+					},
+					Payload: nil,
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &GitlabProvider{
+				secret: tt.fields.secret,
+			}
+			if got := p.Validate(tt.args.hook); got != tt.want {
+				t.Errorf("GitlabProvider.Validate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
