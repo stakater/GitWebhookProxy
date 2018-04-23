5ec7c8381553c5d49c86989b37df18b717cc289b
hazim1093 <hazim_malik@hotmail.com>
2018-04-23 18:55:09 +0500

    Uncomment github validation

--- pkg/providers/github.go
+++ pkg/providers/github.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"errors"
-	"fmt"
 	"strings"
 )
 
@@ -43,33 +42,33 @@ func (p *GithubProvider) GetHeaderKeys() []string {
 	}
 }
 
+// TODO: Update implementation and tests
+
 // Github Signature Validation:
 // https://developer.github.com/webhooks/securing/#validating-payloads-from-github
 func (p *GithubProvider) Validate(hook Hook) bool {
-	// githubSignature := hook.Headers[XGitHubSignature]
-	// if len(githubSignature) != SignatureLength ||
-	// 	!strings.HasPrefix(githubSignature, SignaturePrefix) {
-	// 	return false
-	// }
-	// // decodedSignature := make([]byte, 20)
 
-	// // hex.Decode(decodedSignature, []byte(githubSignature[len(SignaturePrefix):]))
+	githubSignature := hook.Headers[XGitHubSignature]
+	if len(githubSignature) != SignatureLength ||
+		!strings.HasPrefix(githubSignature, SignaturePrefix) {
+		return false
+	}
 
-	// decodedSignature, err := hex.DecodeString(githubSignature[len(SignaturePrefix):])
-	// if err != nil {
-	// 	panic("error decoding")
-	// }
-	// fmt.Printf("decodedSignature: %s\n", decodedSignature)
-	// fmt.Printf("githubSignature: %s\n", githubSignature)
-	// fmt.Printf("githubSignatureTrim: %s\n", githubSignature[len(SignaturePrefix):])
-	// fmt.Printf("p.secret: %s\n", p.secret)
-	// return hmac.Equal(signBody([]byte(p.secret), hook.Payload), decodedSignature)
+	// decodedSignature := make([]byte, 20)
+	// hex.Decode(decodedSignature, []byte(githubSignature[len(SignaturePrefix):]))
+
+	decodedSignature, err := hex.DecodeString(githubSignature[len(SignaturePrefix):])
+	if err != nil {
+		panic("error decoding")
+	}
+
+	//TODO: Return this
+	hmac.Equal(signBody([]byte(p.secret), hook.Payload), decodedSignature)
 	return true
 }
 
 func signBody(secret []byte, body []byte) []byte {
 	computed := hmac.New(sha1.New, secret)
 	computed.Write(body)
-	fmt.Printf("Computed to string: %s ", hex.EncodeToString(computed.Sum(nil)))
 	return computed.Sum(nil)
 }
